5a2c9d98a497986ea6dcedc7faf201c66a780380
Trevor <trevorhreed@gmail.com>
2019-03-12 15:06:32 -0600

    another update

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 export { create, compact, simple, toUuid };
 
-declare function create({ uuid: string = null, base: number = 64 } = {})
+declare function create({ uuid: string, base: number })
 declare function compact()
 declare function simple()
 declare function toUuid(pid: string, base: number)
