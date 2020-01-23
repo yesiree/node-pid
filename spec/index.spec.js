5747ee34e5215ba894ff465f925800dfc6cae67e
Trevor <trevorhreed@users.noreply.github.com>
2020-01-22 23:21:35 -0700

    adding coverage

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,9 +1,6 @@
 describe('Pid', () => {
-  const { create, compact, simple, toUuid, computeLengthFromBase } = require('../lib/index')
-
-  beforeEach(function () {
-
-  })
+  const { createPid, compactPid, simplePid, pidToUuid, computeLengthFromBase } = require('../lib/index')
+  // beforeEach(function () { })
 
   it(`should be able to convert from UUID to PID and back to UUID in multiple bases`, () => {
     const bases = [64, 36, 10, 2]
@@ -23,8 +20,8 @@ describe('Pid', () => {
     bases.forEach(base => {
       const length = computeLengthFromBase(base)
       uuids.forEach(uuid => {
-        const pid = create({ uuid, base })
-        const inverse = toUuid(pid, base)
+        const pid = createPid({ uuid, base })
+        const inverse = pidToUuid(pid, base)
         expect(pid.length).toEqual(length)
         expect(inverse.length).toEqual(36)
         expect(uuid).toEqual(inverse)
@@ -52,8 +49,8 @@ describe('Pid', () => {
     bases.forEach(base => {
       const length = computeLengthFromBase(base)
       pids[base].forEach(pid => {
-        const uuid = toUuid(pid, base)
-        const inverse = create({ uuid, base })
+        const uuid = pidToUuid(pid, base)
+        const inverse = createPid({ uuid, base })
         expect(uuid.length).toEqual(36)
         expect(inverse.length).toEqual(length)
         expect(pid).toEqual(inverse)
@@ -61,23 +58,27 @@ describe('Pid', () => {
     })
   })
 
-  it(`should be able to call 'compact' and get a pid with length 22`, () => {
-    const uuids = [
-      'ffffffff-ffff-ffff-ffff-ffffffffffff',
-      '00000000-0000-0000-0000-000000000000',
-      '528f4e50-1f26-4067-8c9d-8475a0da3e16',
-      '275c7d5b-ec7b-4a04-afe0-00e7602747e2',
-      'e84d040d-5260-4646-bda3-56161f62525e',
-      '52cf29d6-2b61-4c9d-abaf-9d1e7d676acd',
-      'f0f64bda-9afe-4443-a0aa-a74298a1e4d1',
-      'a20a37aa-6a2d-47b3-ad77-56a289e52a8a',
-      'd927d185-2214-4782-8699-6abb136880af',
-      '1e17729d-3fdd-4bec-8562-d514da77cff5'
+  it(`should be able to call createPid() with any combination of parameters`, () => {
+    const base = 36
+    const uuid = '528f4e50-1f26-4067-8c9d-8475a0da3e16'
+    const pids = [
+      { pid: createPid(), base: 64, uuid: null },
+      { pid: createPid(uuid), base: 64, uuid },
+      { pid: createPid({ uuid }), base: 64, uuid },
+      { pid: createPid({ uuid, base }), base, uuid },
+      { pid: createPid({ base }), base, uuid: null }
     ]
-    uuids.forEach(uuid => expect(compact(uuid).length).toEqual(22))
+    pids.forEach(data => {
+      const { pid, base, uuid } = data
+      const length = computeLengthFromBase(base)
+      expect(pid.length).toEqual(length)
+      if (uuid) {
+        expect(pidToUuid(pid, base)).toEqual(uuid)
+      }
+    })
   })
 
-  it(`should be able to call 'simple' and get a pid with length 25`, () => {
+  it(`should be able to call 'compact' and get a PID with length 22`, () => {
     const uuids = [
       'ffffffff-ffff-ffff-ffff-ffffffffffff',
       '00000000-0000-0000-0000-000000000000',
@@ -90,7 +91,64 @@ describe('Pid', () => {
       'd927d185-2214-4782-8699-6abb136880af',
       '1e17729d-3fdd-4bec-8562-d514da77cff5'
     ]
-    uuids.forEach(uuid => expect(simple(uuid).length).toEqual(25))
+    uuids.forEach(uuid => expect(compactPid(uuid).length).toEqual(22))
+  })
+
+  it(`should be able to call 'simple' and get a PID with length 25`, () => {
+    const uuids = [
+      'ffffffff-ffff-ffff-ffff-ffffffffffff',
+      '00000000-0000-0000-0000-000000000000',
+      '528f4e50-1f26-4067-8c9d-8475a0da3e16',
+      '275c7d5b-ec7b-4a04-afe0-00e7602747e2',
+      'e84d040d-5260-4646-bda3-56161f62525e',
+      '52cf29d6-2b61-4c9d-abaf-9d1e7d676acd',
+      'f0f64bda-9afe-4443-a0aa-a74298a1e4d1',
+      'a20a37aa-6a2d-47b3-ad77-56a289e52a8a',
+      'd927d185-2214-4782-8699-6abb136880af',
+      '1e17729d-3fdd-4bec-8562-d514da77cff5'
+    ]
+    uuids.forEach(uuid => expect(simplePid(uuid).length).toEqual(25))
+  })
+
+  it(`should throw an error if createPid is called with a base smaller than 2 or greater than 64`, () => {
+    expect(() => {
+      createPid({ base: 65 })
+    }).toThrowError('Invalid base: 65. Must be between 2 and 64.')
+
+    expect(() => {
+      createPid({ base: 1 })
+    }).toThrowError('Invalid base: 1. Must be between 2 and 64.')
+
+  })
+
+  it(`should throw an error if pidToUuid is called with a PID that exceeds the maximum value for a given base`, () => {
+    const invalidPid = 'f3f3f3f3f3f3f3f3f3f3f3f3f3'
+    expect(() => {
+      pidToUuid(invalidPid, 64)
+    }).toThrowError(`Invalid PID '${invalidPid}'. Maximum PID value using base '64' is '3_____________________'.`)
+  })
+
+  it(`should throw an error if either parameter to pidToUuid is invalid`, () => {
+    expect(() => {
+      pidToUuid()
+    }).toThrowError(`Must provide non-empty string for pid parameter. Found 'undefined' (undefined).`)
+
+    expect(() => {
+      pidToUuid('')
+    }).toThrowError(`Must provide non-empty string for pid parameter. Found '' (string).`)
+
+    expect(() => {
+      pidToUuid('abcd123efgh456ijkl789mnop')
+    }).toThrowError(`Must provide number between 2 and 64 for base parameter. Found 'undefined'.`)
+
+    expect(() => {
+      pidToUuid('abcd123efgh456ijkl789mnop', 1)
+    }).toThrowError(`Must provide number between 2 and 64 for base parameter. Found '1'.`)
+
+    expect(() => {
+      pidToUuid('abcd123efgh456ijkl789mnop', 65)
+    }).toThrowError(`Must provide number between 2 and 64 for base parameter. Found '65'.`)
+
   })
 
 })
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import uuidv4 from 'uuid/v4'
 
 const ALPHABET = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_'
 const UUID_BASE16 = 16
+const DEFAULT_BASE = 64
 const RE_UUID_NO_HYPHEN = /^([a-f0-9]{8})([a-f0-9]{4})([a-f0-9]{4})([a-f0-9]{4})([a-f0-9]{12})$/
 const RE_UUID_WITH_HYPHEN = '$1-$2-$3-$4-$5'
 
@@ -20,7 +21,12 @@ export const create = (uuidOrConfig?: string | PidConfig) => {
   )
     .replace(/-/g, '')
     .toLowerCase()
-  const base = uuidOrConfig ? (typeof uuidOrConfig === 'string' ? 64 : uuidOrConfig.base || 64) : 64
+
+  const base = uuidOrConfig
+    ? (typeof uuidOrConfig === 'string'
+      ? DEFAULT_BASE
+      : uuidOrConfig.base || DEFAULT_BASE
+    ) : DEFAULT_BASE
 
   if (base > ALPHABET.length || base < 2) {
     throw new Error(`Invalid base: ${base}. Must be between 2 and ${ALPHABET.length}.`)
@@ -36,6 +42,14 @@ export const computeLengthFromBase = (base: number): number => {
 }
 
 export const toUuid = (pid: string, base: number) => {
+  if (typeof pid !== 'string' || !pid.length) {
+    throw new Error(`Must provide non-empty string for pid parameter. Found '${pid}' (${typeof pid}).`)
+  }
+
+  if (typeof base !== 'number' || base < 2 || base > 64) {
+    throw new Error(`Must provide number between 2 and 64 for base parameter. Found '${base}'.`)
+  }
+
   const value = BigInt(pid, base, ALPHABET, true)
     .toString(UUID_BASE16, ALPHABET)
     .padStart(32, '0')
@@ -48,7 +62,7 @@ export const toUuid = (pid: string, base: number) => {
   return value.replace(RE_UUID_NO_HYPHEN, RE_UUID_WITH_HYPHEN)
 }
 
-export const compact = () => create({ base: 64 })
+export const compact = () => create({ base: DEFAULT_BASE })
 export const simple = () => create({ base: 36 })
 
 export const createPid = create
